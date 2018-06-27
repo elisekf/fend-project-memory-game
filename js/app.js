0853af210aa9eb95ebc09ba9790ea91104b93f5f
Elise Korsmo Fjeld <elisekorsmofjeld@gmail.com>
2018-06-27 14:28:23 +0200

    refactor: Make turnCard into outside function
    The function turnCard which was previously found inside
    addEventListener is now moved outside, so that the code doesn't have
    to be reproduced several times and is more efficient.

--- js/app.js
+++ js/app.js
@@ -27,16 +27,23 @@ function shuffle(array) {
 
 
 // Adds event listeners to all cards so that they turn when clicked
+function turnCard(index) {
+    cardsArray[index].classList.toggle('open');
+    cardsArray[index].classList.toggle('show');
+}
+
 let cards = document.getElementsByTagName('li');
 const cardsArray = Array.prototype.slice.call(cards);
 
+const openCards = document.getElementsByClassName('open');
+const openCardsArray = Array.prototype.slice.call(openCards);
+
 for (let i = 0; i < cardsArray.length; i++) {
     cardsArray[i].addEventListener('click', function() {
-        cardsArray[i].classList.toggle('open');
-        cardsArray[i].classList.toggle('show');
+        turnCard(i);
     });
 }
-
+  
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
